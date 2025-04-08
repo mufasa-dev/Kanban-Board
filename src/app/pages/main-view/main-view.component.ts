034bdf2250f4daca183c09a384d28e298c5d2e08
mufasa-dev <bruno.ribeiro96@hotmail.com.br>
2025-04-08 09:00:49 -0300

    add drag and drop

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CdkDrag, CdkDragDrop, CdkDropList, CdkDropListGroup, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop'
 
 @Component({
   selector: 'app-main-view',
-  imports: [],
+  imports: [ CdkDrag, CdkDropList, CdkDropListGroup ],
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.scss'
 })
 export class MainViewComponent {
+  public todo = [
+    'Go to Work',
+    'Go home',
+    'Fall sleep'
+  ];
+  public done = [
+    'Get up',
+    'Brush teeth',
+    'Take shower'
+  ];
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
